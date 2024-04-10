681cb5a6b7a7bf7b0190d9b6ad1b5ce3d978df60
Jakub Antoniak <jakant484@gmail.com>
2024-04-10 13:07:19 +0200

    feat : Add firebase dependencies and configure firebase app

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.google.firebase:firebase-admin:9.1.1")
     implementation("com.auth0:java-jwt:4.4.0")
 
     compileOnly("org.projectlombok:lombok")
--- src/main/kotlin/com/example/chat_app/ChatAppApplication.kt
+++ src/main/kotlin/com/example/chat_app/ChatAppApplication.kt
@@ -1,11 +1,30 @@
 package com.example.chat_app
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.io.FileInputStream
 
 @SpringBootApplication
 class ChatAppApplication
 
 fun main(args: Array<String>) {
 	runApplication<ChatAppApplication>(*args)
+}
+
+
+@Bean
+fun firebaseApp(): FirebaseApp {
+
+	val serviceAccount = FileInputStream("src/main/resources/static/account_key.json")
+	val options = FirebaseOptions.builder()
+		.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+		.setDatabaseUrl("https://console.firebase.google.com")
+		.setStorageBucket("chat-b62d0.appspot.com")
+		.build()
+	val firebaseApp = FirebaseApp.initializeApp(options,"appName");
+	return firebaseApp
 }
