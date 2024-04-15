8111af49ecddda6a1e4704f18cc5cdcbb9b90852
Jakub Antoniak <jakant484@gmail.com>
2024-04-15 17:06:06 +0200

    feat : Add WebsocketConfig

---
+++ src/main/kotlin/com/example/chat_app/websockets/ChatController.kt
@@ -0,0 +1,11 @@
+package com.example.chat_app.websockets
+
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.stereotype.Controller
+
+
+@Controller
+class ChatController {
+
+
+}
